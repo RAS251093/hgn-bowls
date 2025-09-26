fc72bf594c24027960195f86d3b7ef0378b5bdd1
Rob Smith <r.smith251093@gmail.com>
2025-09-26 21:53:37 +0100

    biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
+	site: "https://example.com",
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare(),
 });
