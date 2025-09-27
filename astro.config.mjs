4f0eec5248b68779cab1c825c0e05c28d5e2951a
Rob Smith <r.smith251093@gmail.com>
2025-09-27 21:11:20 +0100

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare(),
