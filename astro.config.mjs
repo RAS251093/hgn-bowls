3df994f6608a4dad1651ad8688f71508ef48d8c1
Rob Smith <r.smith251093@gmail.com>
2025-09-30 23:45:37 +0100

    not using tailwind right now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import markdownIntegration from '@astropub/md';
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
-		plugins: [tailwindcss()],
+		plugins: [],
 	},
 	site: "https://example.com",
 	integrations: [mdx(), sitemap(), markdownIntegration()],
